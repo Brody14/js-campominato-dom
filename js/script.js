88290ec6a7d8aa5b78b3a2d68d61749867484680
Brody14 <stefania.spinas@gmail.com>
2023-02-10 16:39:58 +0100

    FEAT bomb check

--- js/script.js
+++ js/script.js
@@ -6,19 +6,15 @@ const gridElement = document.querySelector(".grid");
 
 btnPlayElement.addEventListener("click", startGame);
 //console.log('click');
+const bomb = [];
 
-
-
-//Quando l’utente clicca su una cella: se il numero della cella è presente nella lista dei numeri generati
-//- abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-//Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
 //La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile
 //di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato
 //su una cella che non era una bomba.
 
-let level = 10
-let cellNumber
+let level = 10;
+let cellNumber;
 
 //FUNZIONI
 
@@ -29,16 +25,15 @@ function startGame() {
 	//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta:
 
 	bombGenerator();
-	
+
 	const cellElements = document.querySelectorAll(".cell");
-	
+
 	for (let i = 0; i < cellElements.length; i++) {
 		const cell = cellElements[i];
+
+		cell.addEventListener("click", bombCheck);
 		
-		cell.addEventListener("click", function () {
-			console.log(i + 1);
-			cell.classList.add("bg-violet");
-		});
+
 	}
 	//console.log(cellElements);
 }
@@ -68,7 +63,6 @@ function gridGenerator(gridSide) {
 		//ogni casella deve contenere il numero corrispondente a partire da 1 fino a 100
 		cellElement.innerHTML = cellInner;
 	}
-
 }
 
 function resetGame() {
@@ -83,8 +77,6 @@ function randomNumber(min, max) {
 }
 
 function bombGenerator() {
-	const bomb = [];
-
 	while (bomb.length < 16) {
 		let bombRandom = randomNumber(1, cellNumber);
 		//facendo attenzione che: nella stessa cella può essere posizionata al massimo una bomba,
@@ -92,6 +84,23 @@ function bombGenerator() {
 		if (!bomb.includes(bombRandom)) {
 			bomb.push(bombRandom);
 		}
-		console.log(bomb);
+		//console.log(bomb);
+	}
+}
+
+function bombCheck(event) {
+	//console.log(event, event.target);
+	const cell = event.target;
+	const cellValue = parseInt(event.target.innerHTML);
+	//console.log(cellClass)
+
+	//Quando l’utente clicca su una cella: se il numero della cella è presente nella lista dei numeri generati
+	//- abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+	//Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+	cell.classList.add("bg-azure");
+
+	if (bomb.includes(cellValue)) {
+		cell.classList.add("bg-red");
 	}
 }
